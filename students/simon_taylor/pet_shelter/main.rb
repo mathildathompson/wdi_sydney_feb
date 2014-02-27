fe1e526dcc9ab583e05512b17d748063b2b4be6b
Nizmox <nizmox@gmail.com>
2014-02-28 10:27:05 +1100

    added more comments

--- students/simon_taylor/pet_shelter/main.rb
+++ students/simon_taylor/pet_shelter/main.rb
@@ -1,5 +1,10 @@
+# Stuff to add...
+# - check for valid gender input
+# - adopt / give up should check names are valid...
+
 require 'pry'
 
+#include required .rb files
 require_relative 'shelter'
 require_relative 'client'
 require_relative 'animal'
@@ -30,7 +35,7 @@ shelter.adopt("Nix","Rover")
 
 loop do
 
-#prompt the user for input
+  #print out available options
   puts "1. Create an animal"
   puts "2. Create a client"
   puts "3. Print animals"
@@ -38,13 +43,18 @@ loop do
   puts "5. Adopt animal"
   puts "6. Give up animal"
   puts "7. Quit"
+  puts "---------------------------------"
   puts "Please input a number corresponding to the option you desire."
 
+  #prompt the user for input and convert to integer. 
+  #if they input an invalid option it will be 0
   input = gets.chomp.to_i
 
   puts "---------------------------------"
 
   case input
+
+  #create an animal, create it's toy(s) and add it to the shelter
   when 1
     puts "Please input the animal's name"
     name = gets.chomp
@@ -62,6 +72,8 @@ loop do
 
     animal = Animal.new(name, age, gender, species)
 
+    #start the toys loop so that multiple toys can be input 
+    #this repeats till the user inputs nothing and presses enter
     if toys_yn == 'y'
       loop do
         puts "Please enter the name of the toy (leave blank and press enter to exit)"
@@ -77,6 +89,7 @@ loop do
 
     shelter.animals[name] = animal
 
+  #create a client and then add to the shelter
   when 2
     puts "Please input your name"
     name = gets.chomp
@@ -89,11 +102,15 @@ loop do
 
     shelter.clients[name] = client
 
+  #print animals
   when 3
       shelter.print_animals
+
+  #print clients
   when 4
       shelter.print_clients
 
+  #adopt an animal
   when 5
     puts "Please input your name"
     name = gets.chomp
@@ -102,6 +119,7 @@ loop do
 
     shelter.adopt(name, animal)
 
+  #give up an animal
   when 6
     puts "Please input your name"
     name = gets.chomp
@@ -110,10 +128,12 @@ loop do
 
     shelter.give_up(name, animal)
 
+  #exit command
   when 7
     puts "Exiting program..."
     break
 
+  #invalid option has been input
   else
     puts "Please input a valid option!"
   end
--- students/simon_taylor/pet_shelter/shelter.rb
+++ students/simon_taylor/pet_shelter/shelter.rb
@@ -19,14 +19,20 @@ class Shelter
   end
 
   def adopt(person, pet)
+    #find the animal object
     animal = @animals[pet]
+    #add to the specified client the animal object
     @clients[person].pets[animal.name] = animal
+    #remove this animal from the shelter
     @animals.delete(pet)
   end
 
   def give_up(person, pet)
+    #find the animal object owned by the client
     animal = @clients[person].pets[pet]
+    #add this animal to the shelter
     @animals[pet] = animal
+    #remove this animal from the client
     @clients[person].pets.delete(pet)
   end
 end
