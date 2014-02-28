bcfafee5c106c69fb99715c71a31d28bb405cd31
Anne Homann <an.hmnn@gmail.com>
2014-02-28 11:39:59 +1100

    updated happitails project

--- students/anne_homann/happitails_app/main.rb
+++ students/anne_homann/happitails_app/main.rb
@@ -49,9 +49,9 @@ while response != 'q'
         toys = gets.chomp
 
 
-        shelter.animals << Animal.new(name, age, gender, species, toys)
+        shelter.animals[name] = Animal.new(name, age, gender, species, toys)
 
-        puts "You added an animal. #{shelter.animals.last}"
+        puts "You added an animal. #{shelter.animals[name]}"
         
     elsif response == 'c'
 
@@ -68,9 +68,9 @@ while response != 'q'
         pets = gets.chomp.to_i
 
         
-        shelter.clients << Client.new(name, age, children, pets)
+        shelter.clients[name] = Client.new(name, age, children, pets)
 
-        puts "You added an client. #{shelter.clients.last}"
+        puts "You added an client. #{shelter.clients[name]}"
 
     elsif response == 'd'
 
@@ -80,6 +80,7 @@ while response != 'q'
 
         end    
 
+    elsif response == 'l'
         shelter.clients.each do |bar|
 
             puts bar    
@@ -88,73 +89,87 @@ while response != 'q'
     elsif response == 'o'
 
 
-            puts "Here is a list of clients "
-            shelter.clients.each do |foo|
-                puts foo
+        puts "Here is a list of clients "
+        shelter.clients.each do |foo|
+            puts foo
 
-            end
+        end
 
-            puts "Which client wants to adopt? "
-            AdoptingClient = gets.chomp
-  
-
-            puts "___________________________________________________"
-            puts "Here is a list of animals: "
-            shelter.animals.each do |foo|
-                puts foo
-
-            end
-
-            puts "___________________________________________________"
-            puts "Please type the name of the animal they want to adopt: "
-            adoptionAnimal = gets.chomp
+        puts "Which client wants to adopt? "
+        adopting_client = gets.chomp
 
 
-            if shelter.animals.find(adoptionAnimal)
-                puts "#{AdoptingClient} has adopted #{adoptionAnimal}! Thanks #{AdoptingClient}!"
-                puts "___________________________________________________"
-                # adoptionNew = shelter.animals.delete(adoptionAnimal)
-                
-                shelter.client[AdoptingClient].pets << adoptionAnimal
+        puts "___________________________________________________"
+        puts "Here is a list of animals: "
+        shelter.animals.each do |foo|
+            puts foo
 
-            end
+        end
+
+        puts "___________________________________________________"
+        puts "Please type the name of the animal they want to adopt: "
+        adoptionAnimal = gets.chomp
+
+
+        animal = shelter.animals[adoptionAnimal]
+        shelter.animals.delete(adoptionAnimal)
+        puts "#{adopting_client} has adopted #{adoptionAnimal}! Thanks #{adopting_client}!"
+        puts "___________________________________________________"
+        # adoptionNew = shelter.animals.delete(adoptionAnimal)
+        
+        shelter.clients[adopting_client].pets[adoptionAnimal] = animal
+
 
 
     elsif response == 'g'
 
-            puts "Here is a list of clients "
-            shelter.clients.each do |foo|
-                puts foo
+        puts "Here is a list of clients "
+        shelter.clients.each do |foo|
+            puts foo
 
-            end
+        end
 
-            puts "Which client wants to surrender an animal? "
-            SurrenderClient = gets.chomp
-            puts "Thanks #{SurrenderClient}"
-            puts "___________________________________________________"
-                    
+        puts "Which client wants to surrender an animal? "
+        surrender_client = gets.chomp
+
+        puts "___________________________________________________"
+        puts "Here is a list of #{surrender_client}'s animals: "
+        shelter.clients[surrender_client].pets.each do |foo|
+            puts foo
+
+        end
+
+        puts "___________________________________________________"
+        puts "Which animal would #{surrender_client} like to surrender? "
+        surrendered_animal = gets.chomp
+
+        animal = shelter.clients[surrender_client].pets[surrendered_animal]
+        shelter.clients[surrender_client].pets.delete(surrendered_animal)
+        shelter.animals[surrendered_animal] = animal
+        puts "Thanks #{surrender_client} for giving us #{surrendered_animal}"
+        puts "___________________________________________________"
+               
+
+        # if response == 'q'
+        #     Kernal.exit(true)
+        # end     
 
     end
 
 
-            puts "Would you like to: "
-            puts "add an (a)nimal or a (c)lient"
-            puts "(d)isplay all clients and (l)ist all animals"
-            puts "(o)wn for adopting an animal"
-            puts "(g)iven up for adoption"
-            puts "or (q)uit?: "
-            response = gets.chomp.downcase
+    puts "Would you like to: "
+    puts "add an (a)nimal or a (c)lient"
+    puts "(d)isplay all clients and (l)ist all animals"
+    puts "(o)wn for adopting an animal"
+    puts "(g)iven up for adoption"
+    puts "or (q)uit?: "
+    response = gets.chomp.downcase
 
     
 end
             
 
 
-
-
-
-puts animal
-puts client
 
 
 
