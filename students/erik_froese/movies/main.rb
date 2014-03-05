897e8c2fdbdc0cdc12a2157b351ca39f8649f497
Erik Froese <froesecom@gmail.com>
2014-03-05 20:20:51 +1100

    Movies part 2

--- students/erik_froese/movies/main.rb
+++ students/erik_froese/movies/main.rb
@@ -8,20 +8,47 @@ require 'json'
 get '/movies' do
    #this creates an instance variable @stock_name from user input. params are a hash where the key "stock" from <input name="stock" type="text" placeholder="stocks should show here">. It never changes. The value is the user input, which changes.
     if params[:movie_name]
-      url = "http://www.omdbapi.com/?t=#{params[:movie_name].gsub(' ', '+')}"
+      url = "http://www.omdbapi.com/?s=#{params[:movie_name].gsub(' ', '+')}" 
       response = HTTParty.get(url) #you only need @, if you want to use the variable elsewhere. The response variable will never exist anywhere else
-      @movie = JSON(response)
-      @awards = @movie["Awards"]
-      @director = "Director: #{@movie["Director"]}"
-      @movie_title = @movie["Title"]
-      @plot_header = "Plot"
-      @plot = @movie["Plot"]
-      @poster = @movie['Poster'] 
-      @released = "was released on #{@movie['Released']}."
-      @writer = "Writers: #{@movie["Writer"]}"
+      json = JSON(response)
+      @movies = json["Search"]
     else
       @blank_page = "Please choose a movie above."
+    end
 
-  end
   erb :movies
 end
+
+
+get '/titles/:title' do
+   params[:title]
+    url = "http://www.omdbapi.com/?t=#{params[:title].gsub(' ', '+')}"
+    response = HTTParty.get(url)
+    @movie = JSON(response)
+    @awards = @movie["Awards"]
+    @director = "Director: #{@movie["Director"]}"
+    @movie_title = @movie["Title"]
+    @plot_header = "Plot"
+    @plot = @movie["Plot"]
+    @poster = @movie['Poster'] 
+    @released = "was released on #{@movie['Released']}."
+    @writer = "Writers: #{@movie["Writer"]}" 
+  
+  erb :titles
+end
+
+get '/contact' do
+  erb :contact
+end
+
+get '/useless' do
+  erb :useless
+end
+
+
+
+
+
+
+
+#/titles/:imdbID
