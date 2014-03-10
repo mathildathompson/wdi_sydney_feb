5ebfdfbb2ab35ac8dc41ec95feac13866a9c21aa
Erik Froese <froesecom@gmail.com>
2014-03-10 21:21:42 +1100

    added movies 4 and syled butterflyies

--- students/erik_froese/movies/main.rb
+++ students/erik_froese/movies/main.rb
@@ -1,9 +1,9 @@
 require 'pry'
-#require 'pry-debugger'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'httparty'
 require 'json'
+require 'sqlite3'
 
 get '/movies' do
    #this creates an instance variable @stock_name from user input. params are a hash where the key "stock" from <input name="stock" type="text" placeholder="stocks should show here">. It never changes. The value is the user input, which changes.
@@ -22,17 +22,25 @@ end
 
 get '/titles/:title' do
 
-  #this opens the file and converts the data IN MEMORY (not in the file into a format we can use)
-  #Because of the way the info is stored in the file, you have to convert it to a hash.
-  f = File.open("movies.csv", 'r')
-  #this creates and empty hash to store the info in a hash format
-  @movies = {}
-  #this goes through each line of the file and converts it to a hash IN MEMORY, not in the file
-  f.each do |line|
-    movie = JSON(line)
-    #this manipulates that data so the key is the movie name
-    @movies[ movie['Title'].downcase ] = movie
-   end
+  db = SQLite3::Database.open('movies.db')
+  @movie = db("SELECT * FROM movies WHERE movie_title=#{params[:title]}")
+  #the below is what's returned for "Alien". Somehow, I have to manipulate it into useable variables.
+  #2|Won 1 Oscar. Another 12 wins & 18 nominations.|Director: Ridley Scott|Alien|The commercial vessel Nostromo receives a distress call from an unexplored planet. After searching for survivors, the crew heads home only to realize that a deadly bioform has joined them.|http://ia.media-imdb.com/images/M/MV5BMTk3NzkwMjA3OV5BMl5BanBnXkFtZTYwMTIwOTk2._V1_SX300.jpg|was released on 22 Jun 1979.|Writers: Dan O Bannon (story), Ronald Shusett (story), Dan O Bannon (screenplay)
+  @movie.split("|") do |x|
+      movies = []
+      movies.push(x)
+
+      @awards = movies(1)
+      @director = movies(2)
+      @movie_title = movies(3)
+      @plot_header = movies(4)
+      @plot = movies(5)
+      @poster = movies(6)
+      @released = movies(7)
+      @writer = movies(8)
+    end
+  
+
 
    @movie = @movies[ params[:title].downcase ]
 
@@ -41,10 +49,6 @@ get '/titles/:title' do
       url = "http://www.omdbapi.com/?t=#{params[:title].gsub(' ', '+')}"
       response = HTTParty.get(url)
       @movie = JSON(response)
-        f = File.new("movies.csv", 'a+')
-        f.puts(response)
-        f.close   
-    ends
       @awards = @movie["Awards"]
       @director = "Director: #{@movie["Director"]}"
       @movie_title = @movie["Title"]
@@ -52,7 +56,15 @@ get '/titles/:title' do
       @plot = @movie["Plot"]
       @poster = @movie['Poster'] 
       @released = "was released on #{@movie['Released']}."
-      @writer = "Writers: #{@movie["Writer"]}"   
+      @writer = "Writers: #{@movie["Writer"]}" 
+  
+      db = SQLite3::Database.open('movies.db')
+      db.results_as_hash = true
+      sql = "INSERT INTO movies (awards, director, movie_title, plot, poster, released, writer) VALUES ('#{@awards.gsub("'", " ")}', '#{@director.gsub("'", " ")}', '#{@movie_title.gsub("'", " ")}', '#{@plot.gsub("'", " ")}', '#{@poster.gsub("'", " ")}', '#{@released.gsub("'", " ")}', '#{@writer.gsub("'", " ")}');"
+      db.execute(sql)
+    
+    end
+        
   
   erb :titles
 end
@@ -68,7 +80,3 @@ end
 
 
 
-
-
-
-#/titles/:imdbID
