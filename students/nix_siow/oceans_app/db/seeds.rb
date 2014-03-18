dd66184263fa27e0defc48e35136fcd266e3c991
nixsiow <nixsiow@hotmail.com>
2014-03-19 10:34:58 +1100

    homework, OceanApp - Nix

---
+++ students/nix_siow/oceans_app/db/seeds.rb
@@ -0,0 +1,5 @@
+Ocean.destroy_all
+
+Ocean.create({:name => 'indian_ocean', :image => "indian_ocean.jpg", :volume => 123123, :bordering_countries => 'India, Bangladesh', :shipping_routes => 'Australia' })
+Ocean.create({:name => 'Pacific Ocean', :image => "pacific_ocean.jpg", :volume => 31231, :bordering_countries => 'India, Bangladesh, china', :shipping_routes => 'India' })
+Ocean.create({:name => 'Southern Ocean', :image => "southern_ocean.jpg", :volume => 4565345, :bordering_countries => 'India, UK', :shipping_routes => 'UK' })
