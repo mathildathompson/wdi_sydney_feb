19f8c470f45435407c66081d0dfe817b06163c29
markpereira <mark.pereira@xtra.co.nz>
2014-03-24 16:00:16 +1100

    deleted project from homework repo

--- students/mark_pereira/project1_justlike/justlike_app/db/migrate/20140322135458_create_recipes_users.rb
+++
@@ -1,8 +0,0 @@
-class CreateRecipesUsers < ActiveRecord::Migration
-  def change
-    create_table :recipes_users, :id => false do |t|
-    	t.integer :recipe_id
-    	t.integer :user_id
-    end
-  end
-end
--- students/mark_pereira/project1_justlike/justlike_app/db/migrate/20140322140448_create_cuisines_recipes.rb
+++
@@ -1,8 +0,0 @@
-class CreateCuisinesRecipes < ActiveRecord::Migration
-  def change
-    create_table :cuisines_recipes, :id => false do |t|
-    	t.integer :cuisine_id
-    	t.integer :recipe_id
-    end
-  end
-end
