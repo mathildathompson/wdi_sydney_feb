38eb47081bff70b2464d06dea6af0b38abcf0313
Nizmox <nizmox@gmail.com>
2014-04-09 20:42:13 +1000

    final version

--- students/simon_taylor/js_memory/memory.js
+++ students/simon_taylor/js_memory/memory.js
@@ -1,8 +1,12 @@
-//Global Variables
+//------------------------------------------------------------------------------
+// SETUP VARIABLES
+//------------------------------------------------------------------------------
 
 //Time that increments in the game
-var timerId = 0,
+var timerID = 0,
     time = 0;
+
+var $timer
 
 //arrays with letters in them.
 var lettersSmall  = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E'],
@@ -16,10 +20,13 @@ var lettersSmall  = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E'],
 //array of letters. will change based on size of game.
 var letters;
 
-//last card/letter you clicked on. comes from the letter divs.
-var lastID = '',
-    lastLetter = '';
+//to store the game element
+var $game
 
+//to store all cards
+var $cards;
+
+//last card you clicked on
 var $lastCard;
 
 //------------------------------------------------------------------------------
@@ -28,19 +35,31 @@ var $lastCard;
 
 //Code In Here gets executed once code is ready. ie hovering, clicking events//
 $(document).ready(function () {
+
+  $timer = $('#timer');
+
   var $controls = $('#controls');
+
+  $game = $('#game');
 
   $controls.on('click','input',function (event) {
 
     //get the game size from the buttons id
     var gameSize = $(this).attr('id');
+
+    //a game is already in progress so reset first
+    if ($game.children().length > 0) { reset(); }
     
     //run start game and pass in the game size
     startGame(gameSize);
+
+    //setup the game timer
+    startTime();
   });
 
 });
 
+//START EVENT HANDLERS FOR OUR CARDS
 var cardEventsInit = function () {
   //find all the cards (columns)
   var $cards = $('.column');
@@ -65,8 +84,8 @@ var cardEventsInit = function () {
 // GAME FUNCTIONS
 //------------------------------------------------------------------------------
 
-// Initializes the game and creates the board
-function startGame(gameSize) {
+//INITIALIZES THE GAME AND GENERATES THE BOARD
+var startGame = function (gameSize) {
   //get the id attribute from the button clicked
 
   switch (gameSize) {
@@ -82,22 +101,24 @@ function startGame(gameSize) {
   }
 
   //shuffle the letters
-  // letters = _.shuffle(letters);
+  letters = _.shuffle(letters);
 
   //find the #game div to append our cards (columns) to
-  var $game = $('#game');
+  // $game = $('#game');
 
   _.each(letters, function (letter,index) {
     $game.append('<div class="column" id="' + index + '"></div>');
     // console.log(letter,index)
   });
 
+  $cards = $('.column');
+
   cardEventsInit();
   
 }
 
-// Flips a card and checks for a match
-function cardClick($card) {
+// FLIP THE CARD AND CHECK FOR A MATCH
+var cardClick = function ($card) {
   //get the id from the clicked card
   cardID = $card.attr('id');
   //find the letter associated with that card from our array
@@ -105,19 +126,20 @@ function cardClick($card) {
   //display the letter for the clicked card
   $card.text(letter);
 
-  //if the lastID is not empty (it's not the first click)
-  if (lastID !== '') { checkMatch(letter, $card); }
+  //if the lastCard is defined (it's not the first click)
+  if ($lastCard !== undefined) { checkMatch($card, letter); }
 
   //set global variables for next click
-  lastID = cardID;
-  lastLetter = letter;
+  $lastCard = $card;
 
 }
 
-var checkMatch = function (letter, $card) {
-  //find the last card node
-  $lastCard = $('#' + lastID);
-  //update the clicked card with the relevant letter from the letters array
+//CHECK TO SEE IF THERE IS A SUCCESSFUL MATCH
+var checkMatch = function ($card, letter) {
+  //get the id from the previous clicked card
+  var lastCardID = $lastCard.attr('id');
+  //find the letter associated with the previous clicked card from our array
+  var lastLetter = letters[lastCardID];
 
   //this was a correct match
   if (letter === lastLetter) {
@@ -128,15 +150,58 @@ var checkMatch = function (letter, $card) {
   } else if ($lastCard.hasClass('found') === false) {
     $lastCard.text("");
   }
-
+  //check to see if the user has won
+  checkWin();
 };
 
-//Start the timer
-function startTime() {
 
+//CHECK TO SEE IF THE PLAYER HAS WON
+var checkWin = function () {
+
+  //check if they have been revealed
+  var foundCards = _.select($cards, function(card) { return $(card).hasClass('found'); });
+  //check if found cards = 
+  if (foundCards.length === letters.length) {
+    //stop the timer
+    stopTimer();
+    //remove the foudn class from all cards
+    $cards.removeClass('found');
+    //add the won class to all cards
+    $cards.addClass('won');
+    //return true for success
+    return true;
+  } else {
+    //return fals for failure
+    return false;
+  }
+};
+
+//RESET THE GAME SO A NEW GAME CAN BE STARTED
+var reset = function () {
+  //delete all cards
+  $game.empty();
+  //stop the timer
+  stopTimer();
+  //remove the timer text
+  $timer.text("");
+};
+
+//------------------------------------------------------------------------------
+// TIMERS
+//------------------------------------------------------------------------------
+
+//Start the timer
+var startTime = function () {
+  $timer.text("Game Time: 0")
+  timerID = window.setInterval(updateTime, 1000);
 }
 
 //Increment the timer and display the new time
-function updateTime() {
-
+var updateTime = function () {
+  $timer.text(["Game Time:",++time].join(' '));
 }
+
+var stopTimer = function () {
+  time = 0;
+  clearInterval(timerID);
+};
